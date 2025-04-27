60cd6ab68de095bdd135a4758a71b5b5999d4fe1
Qasimmizbah <36446191+qasimmizbah@users.noreply.github.com>
2025-04-27 17:27:49 +0530

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function($) {
-    // Initialize color picker
-    $('.color-picker').wpColorPicker();
+    
+    // Initialize color pickers
+    $('.color-field').wpColorPicker();
+    
+    // Tab switching
+    $('.nav-tab-wrapper a').on('click', function(e) {
+        e.preventDefault();
+        
+        // Remove active class from all tabs
+        $('.nav-tab-wrapper a').removeClass('nav-tab-active');
+        $('.tab-content').removeClass('active');
+        
+        // Add active class to clicked tab
+        $(this).addClass('nav-tab-active');
+        $($(this).attr('href')).addClass('active');
+    });
+    
+    // Show the first tab by default
+    $('.nav-tab-wrapper a:first').trigger('click');
 });
